7dbe3001a10a50671104af1a4aee8cded5e35486
Genevieve Koffman <genkoffman@gmail.com>
2023-12-02 15:00:58 -0500

    skeleton for func we need to implement

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 modifyWhere = async function(uid, where) {
 	// get all groups of user
-	const groups = await prisma.group.findMany({
+	const groups = await prisma.group.findMany({ //where do we create the group table? 
 		where: {
 			uid: uid
 		}
@@ -25,10 +25,34 @@ exports.findFirstOrThrow = async function(table, uid, where) {
 	return await table.findFirstOrThrow(where);
 }
 
+exports.findUniqueOrThrow = async function(table, uid, where) {
+
+}
+
 exports.findMany = async function(table, uid, where) {
 	where = modifyWhere(uid, where['where']);
 	return await table.findMany(where);
 }
 
 exports.create = async function(table, uid, where) {
+	
 }
+
+exports.update = async function(table, uid, where) {
+	
+}
+
+exports.delete = async function(table, uid, where) {
+
+}
+
+exports.deleteMany = async function(table, uid, where) {
+	
+}
+
+exports.upsert = async function(table, uid, where) {
+	
+}
+
+
+
